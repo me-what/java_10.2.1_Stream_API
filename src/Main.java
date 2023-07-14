63b7f55812261bb1c9b33e1aebc1e91e2d120bd6
withoutLastNameAndFirstName <eldar.979751@gmail.com>
2023-07-14 22:58:10 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// // Создание потока данных
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> cities = new ArrayList<String>();
+        Collections.addAll(cities, "Париж", "Лондон", "Мадрид");
+        cities.stream() // получаем поток
+                .filter(s->s.length()==6) // применяем фильтрацию по длине строки
+                .forEach(s->System.out.println(s)); // выводим отфильтрованные строки на консоль
+    }
+}
